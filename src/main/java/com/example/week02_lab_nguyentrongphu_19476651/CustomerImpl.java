929f17f8a681ea7c946973c19061c8c541828e77
trongphu267 <nguyentrgphu112@gmail.com>
2023-12-02 05:54:10 +0700

    Initial commit

---
+++ src/main/java/com/example/week02_lab_nguyentrongphu_19476651/CustomerImpl.java
@@ -0,0 +1,2 @@
+package com.example.week02_lab_nguyentrongphu_19476651;public class CustomerImpl {
+}
---
+++ src/main/java/com/example/week02_lab_nguyentrongphu_19476651/repository/CustomerRepository.java
@@ -0,0 +1,2 @@
+package com.example.week02_lab_nguyentrongphu_19476651.repository;public interface AccountRepository {
+}
